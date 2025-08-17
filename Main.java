e83dbc42ce7754a6953b628a3475c9cab7a4bb94
Dhatchayani U <dhatchayani.240066@aids.ritchennai.edu.in>
2025-08-17 13:38:20 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package assignment;
+
+public class Main {
+	 public static void main(String[] args) {
+		 
+	        Book myBook = new Book("Wings of Fire", "A.P.J. Abdul Kalam", 1999, 350.00);
+
+	        System.out.println(myBook);
+	        System.out.println("Dhatchayani U"); 
+	        System.out.println("2117240070066");
+	    }
+}
